4c11f8683637d55cd8698cc18f98cab701c18abb
antekjakowicz <antoni.jakowicz@cyfroweszkoly.pl>
2025-05-31 22:48:25 +0200

    v0.3
    Probowanie zrobienia jakichs nav graphow i wgl ale nw nic nie dziala pls pomocy w sumie to nw po co pan tyle tego zadal i co pan to bedzie teraz sprawdzal to tam jezu az tyle tych aplikacji szczerze to wspolczuje panu bardzo ale no spoko ok okok ok david jasper michal beczak

--- app/src/main/java/com/example/moodtracker/MainActivity.kt
+++ app/src/main/java/com/example/moodtracker/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+// Połączenie z NavControllerem
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // ToolBar jako ActioBar
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Połączenie ToolBara z NavControllerem
+        setupActionBarWithNavController(navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
